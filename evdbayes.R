972901c0f657beae5c733f92e17ff2323850482f
Reetam Majumder <rmajumd3@ncsu.edu>
2023-12-06 20:28:03 -0500

    LA analysis

--- evdbayes.R
+++ evdbayes.R
@@ -1,11 +1,11 @@
 rm(list = ls())
 library(evd)
+library(evmix)
 library(evdbayes)
-load('HCDN/HCDN_annual_max.RData')
+load('LA.RData')
 
-nmiss = apply(Y,1,function(x)sum(is.na(x)))
-which(nmiss[HUC02=='10L']<22)
-y = Y[HUC02=='10L',][19,]
+tmp = aggregate(tmax,list(years),FUN=max)
+y=tmp[,2]
 # hist(y)
 # load('precip.Rda')
 # y <- as.matrix(maxs[ , 5:ncol(maxs)])[1,]
@@ -14,9 +14,29 @@ mle.fit = fgev(y)
 mle.fit
 mat <- diag(c(10000, 100, 0.25))
 pr.strm <- prior.norm(mean = c(0,0,0), cov = mat, trendsd = 0)
-n <- 15000 ; t0 <- mle.fit$estimate ; s <- mle.fit$std.err ; b <- 5000
+n <- 11000 ; t0 <- mle.fit$estimate ; s <- mle.fit$std.err ; b <- 1000
 # s[2] = log(s[2]); t0[2] = log(t0[2])
-rn.post <- posterior(n, t0, pr.strm, "gev", data = y, burn = b,psd=s)
+rn.post <- posterior(n, t0, pr.strm, "gpd", data = y, burn = b,psd=s)
+summary(rn.post)
+
+par(mfrow=c(1,3))
+plot(1:10001,rn.post[,1],'l')
+plot(1:10001,rn.post[,2],'l')
+plot(1:10001,rn.post[,3],'l')
+par(mfrow=c(1,1))
+
+
+thresh = aggregate(tmax,list(years),FUN=quantile,0.95)
+summary(thresh[,2])
+y = tmax[tmax>99]
+length(y)
+mle.fit = fgpd(y,u=99)
+mle.fit$mle
+mle.fit$se
+
+t0 <- c(95,mle.fit$mle) ; s <- c(.33,mle.fit$se)
+rn.post <- posterior(n, t0, pr.strm, "gpd", data = y, burn = b,psd=s)
+
 summary(rn.post)
 
 par(mfrow=c(1,3))
